d91f756b9f91810fa08c16bbcb0449e5b1cd5016
mststar0510 <mststar0510@yahoo.co.jp>
2018-09-21 23:13:02 +0900

    daily python

--- kadai13.py
+++ kadai13.py
@@ -1,35 +1,33 @@
 def isiterable(word):
     try:
-        word.__iter__(self)
+        getattr(word,'__iter__')
         print('イテラブルです。')
     except:
         print('イテラブルではありません。')
 
 def isiterator(word):
-    #try:
-        first=word[0],
-        for i in word:
-            pass
-        if first==word[0]:
-            print('イテレーターではありません。')
-        elif first!=word[0]:
-            printe('イテレーターです。')
+    try:
+        getattr(word,'__next__')
+        print('イテレーターです。')
     except :
         print('イテレーターではありません。')
 
-list0=[1,2,3,4]
-list1=[1,2,3,4,5,6]
+list=[1,2,3,4]
 word1='a'
 word2=1
-iter1=iter(list0)
-iter2=iter(list1)
-'''isiterable(list0)
-isiterator(list0)
+iter=iter(list)
 isiterable(word1)
 isiterator(word1)
+print('--------------------')
+isiterable(list)
+isiterator(list)
+print('--------------------')
 isiterable(word2)
 isiterator(word2)
-isiterable(list1)
-isiterator(list1)
-isiterable(iter1)'''
-isiterator(iter2)
+print('--------------------')
+isiterable(iter)
+isiterator(iter)
+print('--------------------')
+isiterable(map(int,[]))
+isiterator(map(int,[]))
+print('--------------------')
